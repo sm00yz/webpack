c4dd78da49da5f1e2b24331d67625df095889e38
Samia <alghamdi.smasm@gmail.com>
2020-06-06 00:43:08 +0300

    final commit

--- src/index.js
+++ src/index.js
@@ -2,3 +2,13 @@ import { checkForName } from './client/js/nameChecker.js'
 import { handleSubmit } from './client/js/formHandler.js'
 
 alert("Hi i'm here ");
+
+import './client/styles/resets.css'
+import './client/styles/base.css'
+import './client/styles/form.css'
+import './client/styles/header.css'
+
+export {
+    checkForName,
+    handleSubmit
+}
